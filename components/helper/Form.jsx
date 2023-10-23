3c0ea067288fd78574114a9df7a2e0517e90fa6f
Kex027 <adafisz27@gmail.com>
2023-10-23 17:51:45 +0200

    Added recaptcha to the form

--- components/helper/Form.jsx
+++ components/helper/Form.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import classNames from "classnames";
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Form = ({ style }) => {
+  const [isVerified, setIsVerified] = useState(false);
+  const [showCaptcha, setShowCaptcha] = useState(false);
+  const captchaRef = useRef(null);
+
   const [success, setSuccess] = useState(false);
   const {
     register,
@@ -13,12 +18,22 @@ const Form = ({ style }) => {
   } = useForm();
 
   const onSubmit = (data) => {
+    setShowCaptcha(true);
+    if (!isVerified) {
+      setSuccess(false);
+      return;
+    }
+    data = { ...data, isVerified: isVerified };
+
     axios
       .post("https://sandbox.000.pe/contact.php", data)
       .then((response) => {
         setSuccess(true);
         console.log(`${response.status}: Success!`);
         reset();
+        captchaRef.current.reset();
+        setShowCaptcha(false);
+        setIsVerified(false);
       })
       .catch((err) => {
         setSuccess(false);
@@ -39,7 +54,6 @@ const Form = ({ style }) => {
       {errors.name && (
         <span style={{ color: "#f00" }}>This field is required</span>
       )}
-
       <label htmlFor="email" className={classNames(style.label)}>
         Email *
       </label>
@@ -57,7 +71,6 @@ const Form = ({ style }) => {
       {errors.email && (
         <span style={{ color: "#f00" }}>This field is required</span>
       )}
-
       <label htmlFor="message" className={classNames(style.label)}>
         Message *
       </label>
@@ -72,12 +85,25 @@ const Form = ({ style }) => {
         <span style={{ color: "#f00" }}>This field is required</span>
       )}
 
+      {showCaptcha && (
+        <div style={{ alignSelf: "center" }}>
+          <ReCAPTCHA
+            ref={captchaRef}
+            sitekey={"6LfmvMMoAAAAAIWouPy6IM5NLRgiA2dTZw2weg32"}
+            onChange={(value) => setIsVerified(value)}
+          />
+        </div>
+      )}
+
       <button type={"submit"} className={classNames(style.button)}>
         Submit
       </button>
-
       {success && (
-        <span style={{ color: "#0f0" }}>Submitted successfully!</span>
+        <span
+          style={{ color: "#0f0", fontWeight: "bold", alignSelf: "center" }}
+        >
+          Thank you for getting in touch!
+        </span>
       )}
     </form>
   );
