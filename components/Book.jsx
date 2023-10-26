a60136dda8685b7dde6788428f3775cf3da9d357
Kex027 <adafisz27@gmail.com>
2023-10-26 18:24:13 +0200

    Fixed book size, and overall displaying

--- components/Book.jsx
+++ components/Book.jsx
@@ -1,24 +1,8 @@
 import { forwardRef, useRef, useState } from "react";
-import IntoLeft from "./pages/IntoLeft";
-import IntoRight from "./pages/IntoRight";
-import ForewordLeft from "./pages/ForewordLeft";
-import ForewordRight from "./pages/ForewordRight";
-import ContentsLeft from "./pages/ContentsLeft";
-import ContentsRight from "./pages/ContentsRight";
-import ContextLeft from "./pages/ContextLeft";
-import ContextRight from "./pages/ContextRight";
-import VideoLeft from "./pages/VideoLeft";
-import VideoRight from "./pages/VideoRight";
 import HTMLFlipBook from "react-pageflip";
 import LeftPage from "./LeftPage";
 import RightPage from "./RightPage";
 import GetBookPage from "./helper/GetBookPage";
-// import { useState } from "react";
-// import classNames from "classnames";
-// import style from "../styles/book.module.scss";
-// import DoublePage from "./DoublePage";
-// import Cover from "./Cover";
-// import GetBookPage from "./helper/GetBookPage";
 
 const Book = forwardRef(({ pages, bookmarks }, ref) => {
   // currentPage,
@@ -28,6 +12,7 @@ const Book = forwardRef(({ pages, bookmarks }, ref) => {
   const [qaIndex, setQaIndex] = useState(0);
 
   const [currentPage, setCurrentPage] = useState(0);
+  const [leftPageIndex, setLeftPageIndex] = useState(0);
   const [changeCustomPage, setChangeCustomPage] = useState(0);
 
   const bookRef = useRef(null);
@@ -36,19 +21,21 @@ const Book = forwardRef(({ pages, bookmarks }, ref) => {
   return (
     <HTMLFlipBook
       showCover
-      width={270}
-      height={385}
+      height={770}
+      width={540}
+      // autoSize={false}
       // minWidth={250}
       // maxWidth={500}
       // minHeight={100}
       // maxHeight={330}
       size="stretch"
       maxShadowOpacity={0.5}
+      onFlip={(e) => setLeftPageIndex(e.data)}
       // onFlip={({ object }) => {
       //   setCurrentPage(object.pages.currentSpreadIndex);
       // }}
       style={{
-        transform: currentPage === 0 ? "translateX(-25%)" : "translateX(0%)",
+        transform: leftPageIndex === 0 ? "translateX(-25%)" : "translateX(0%)",
         transition: "transform .75s ease-in-out",
       }}
     >
