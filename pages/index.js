0841fdcb24e027d0fb94c50fab2e604211581b1e
Kex027 <adafisz27@gmail.com>
2023-11-02 10:46:18 +0100

    fixing deployment

--- pages/index.js
+++ pages/index.js
@@ -54,6 +54,15 @@ export default function Index({ bookData }) {
     }
 
     useEffect(() => {
+      document.onkeydown = (e) => {
+        e = e || window.event;
+        if (e.keyCode === 37) {
+          changePage(-1);
+        } else if (e.keyCode === 39) {
+          changePage(1);
+        }
+      };
+
       setTimeout(() => {
         setShowLoader(false);
       }, 250);
@@ -70,15 +79,6 @@ export default function Index({ bookData }) {
     }, []);
     return windowSize;
   }
-
-  document.onkeydown = (e) => {
-    e = e || window.event;
-    if (e.keyCode === 37) {
-      changePage(-1);
-    } else if (e.keyCode === 39) {
-      changePage(1);
-    }
-  };
 
   const changePage = (value) => {
     setCurrentPage((oldValue) => {
